95feb833f8b14c01db7772651b71484a4dea06f9
devsigner9920 <devsigner9920@icloud.com>
2022-04-15 17:09:18 +0900

    fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,7 @@
-import { screen } from '@testing-library/react';
-
 test('1 test: renders learn react link', () => {
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
   console.log("1 passed");
 });
 
 test('2 test: second test', () => {
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
   console.log("2 passed");
 });
